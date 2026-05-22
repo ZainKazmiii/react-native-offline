722aa90d9e5fb196b343ed39cc7ae8c39879a11a
ZainKazmiii <111486944+ZainKazmiii@users.noreply.github.com>
2026-05-22 14:15:05 -0400

    Upgrade example app for current Expo Go: migrate tab navigator

--- example/navigation/MainTabNavigator.tsx
+++ example/navigation/MainTabNavigator.tsx
@@ -1,67 +1,42 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import {
-  createStackNavigator,
-  createBottomTabNavigator,
-} from 'react-navigation';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
 import TabBarIcon from '../components/TabBarIcon';
 import ComponentsScreen from '../screens/ComponentsScreen';
 import ReduxScreen from '../screens/ReduxScreen';
 import SagasScreen from '../screens/SagasScreen';
 
-const ComponentsStack = createStackNavigator({
-  Components: ComponentsScreen,
-});
+const Tab = createBottomTabNavigator();
 
-type TabBarIconArgs = {
-  focused: boolean;
-};
-
-ComponentsStack.navigationOptions = {
-  tabBarLabel: 'Components',
-  tabBarIcon: ({ focused }: TabBarIconArgs) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
-
-const ReduxStack = createStackNavigator({
-  Redux: ReduxScreen,
-});
-
-ReduxStack.navigationOptions = {
-  tabBarLabel: 'Redux',
-  tabBarIcon: ({ focused }: TabBarIconArgs) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-    />
-  ),
-};
-
-const SagasStack = createStackNavigator({
-  Sagas: SagasScreen,
-});
-
-SagasStack.navigationOptions = {
-  tabBarLabel: 'Sagas',
-  tabBarIcon: ({ focused }: TabBarIconArgs) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-    />
-  ),
-};
-
-export default createBottomTabNavigator({
-  ComponentsStack,
-  ReduxStack,
-  SagasStack,
-});
+export default function MainTabNavigator() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => {
+            let iconName: string;
+            if (route.name === 'Components') {
+              iconName =
+                Platform.OS === 'ios'
+                  ? `ios-information-circle${focused ? '' : '-outline'}`
+                  : 'md-information-circle';
+            } else if (route.name === 'Redux') {
+              iconName = Platform.OS === 'ios' ? 'ios-link' : 'md-link';
+            } else {
+              iconName = Platform.OS === 'ios' ? 'ios-options' : 'md-options';
+            }
+            return <TabBarIcon focused={focused} name={iconName} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Components" component={ComponentsScreen} />
+        <Tab.Screen name="Redux" component={ReduxScreen} />
+        <Tab.Screen name="Sagas" component={SagasScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
